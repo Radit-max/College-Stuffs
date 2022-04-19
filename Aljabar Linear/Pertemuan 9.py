23f440b3eac0bdf22d873b74f83bc0c5e6b9e6e1
Raditya-Maxicomp <70063420+Radit-max@users.noreply.github.com>
2022-04-19 14:28:50 +0700

    Update Pertemuan 9.py

--- Aljabar Linear/Pertemuan 9.py
+++ Aljabar Linear/Pertemuan 9.py
@@ -1,3 +1,14 @@
 #jangan lupa untuk pip install numpy
-import math
-import os
+import numpy as np
+from numpy.linalg import det
+from numpy.linalg import eig
+#Misalkan Matriks A :
+A = np.array([[1,6],[5,2]])
+#Mencari nilai eigenvalue dan eigenvector
+#a merupakan eigenvalue dan b merupakan eigenvector
+a,b = eig(A)
+#print hasil yang diinginkan
+print("Determinan dari matriks A adalah",det(A))
+print("Eigen Value :", a)
+print("Eigen Vector :", b)
+#sekian...
